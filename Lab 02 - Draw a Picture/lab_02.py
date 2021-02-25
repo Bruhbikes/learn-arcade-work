b431ec9b8425a9631a1b7e0018c7933ef0f7bebe
ethan <ethan.bff@gmail.com>
2021-02-26 09:26:35 +1300

    drwaing

---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,22 @@
+import arcade
+
+arcade.open_window(800, 600, "drawing")
+
+arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
+
+arcade.start_render()
+
+arcade.draw_triangle_filled(400, 250, 400, 400, 350, 300, arcade.color.WHITE)
+
+arcade.draw_triangle_filled(400, 250, 400, 400, 450, 300, arcade.color.WHITE)
+
+arcade.draw_rectangle_filled(100, 600, 1400, 400, arcade.color.EMERALD)
+
+arcade.draw_rectangle_filled(0, 50, 1600, 200, arcade.color.EMERALD)
+
+arcade.draw_text("pog", 100, 200, arcade.color.GREEN)
+
+arcade.finish_render()
+
+# Keep the window up until someone closes it.
+arcade.run()
