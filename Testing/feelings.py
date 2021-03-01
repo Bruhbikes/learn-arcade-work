3efbd228986a1e943a53d2b498f5a80bc8df3ea6
ethan <ethan.bff@gmail.com>
2021-03-02 10:32:03 +1300

    feelings

--- Testing/feelings.py
+++ Testing/feelings.py
@@ -1,27 +1,62 @@
-print("welcome :)")
+def main():
+    feeling = input("Do you want to start? y/n: ").lower()
+    while feeling == "y":
 
-print("")
+        print("")
 
-# Output a feeling
-print("A: I Feel a bit worried.")
-print("B: I Feel a bit happy.")
-print("C: I Feel a bit angry.")
-print("D: I Feel a bit sad.")
-print("E: I Feel a bit nervous.")
+        # Output a feeling
+        print("A: I Feel a bit worried.")
+        print("B: I Feel a bit happy.")
+        print("C: I Feel a bit angry.")
+        print("D: I Feel a bit sad.")
 
-# Output instructions
-print("Enter the letter that corresponds the most closely to how you feel.")
+        print("")
 
-# Creates a variable called feeling that stores the users input.
-# Notice the space. It's important.
-feeling = input("Choose a feeling from the options (A): ").lower()
+        # Output instructions
+        print("Enter the letter that shows how you feel.")
 
-if feeling == "a":
-    print()
-    print("You can destroy your now, by worrying about tomorrow.")
-    print()
-    print("Janis Joplin")
-    print()
+        print("")
 
-elif feeling == "b":
-    print()
+        # Creates a variable called feeling that stores the users input.
+        # Notice the space. It's important.
+        feeling = input("Choose a feeling from the options: ").lower()
+        print("")
+        if feeling == "a":
+            print()
+            print("You can destroy your now, by worrying about tomorrow.")
+            print()
+            print("Janis Joplin")
+            print()
+
+        elif feeling == "b":
+            print()
+            print("Do not set aside your happiness. Do not wait to be happy in the future. The best time to be happy is "
+                  "always now.")
+            print()
+            print("Roy T. Bennett")
+            print()
+
+        elif feeling == "c":
+            print()
+            print("Anybody can become angry — that is easy, but to be angry with the right person and to the right degree ")
+            print("and at the right time and for the right purpose, and in the right way — that is not within everybody's ")
+            print("power and is not easy.")
+            print()
+            print("Aristotle")
+            print()
+
+        elif feeling == "d":
+            print()
+            print("There are moments when I wish I could roll back the clock and take all the sadness away, but I have ")
+            print("the feeling that if I did, the joy would be gone as well.")
+            print()
+            print("Nicholas Sparks")
+            print()
+
+        feeling = input("Do you want to continue? y/n: ").lower()
+
+        if feeling == "n":
+            print("closing feelings generator. ")
+            print("have a nice day :) ")
+
+main()
