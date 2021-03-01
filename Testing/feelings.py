abe7d96be1cecf1a1aac82ee79137e612f26059a
ethan <ethan.bff@gmail.com>
2021-03-01 14:43:41 +1300

    feelings #1

---
+++ Testing/feelings.py
@@ -0,0 +1,28 @@
+print("welcome :)")
+
+print("")
+
+# Output a feeling
+print("A: I Feel a bit worried.")
+print("B: I Feel a bit worried.")
+print("C: I Feel a bit worried.")
+print("D: I Feel a bit worried.")
+print("E: I Feel a bit worried.")
+
+# Output instructions
+print("Enter the letter that corresponds the most closely to how you feel.")
+
+# Creates a variable called feeling that stores the users input.
+# Notice the space. It's important.
+feeling = input("Choose a feeling from the options (A): ").lower()
+
+if feeling == "a":
+    print()
+    print("You can destroy your now, by worrying about tomorrow.")
+    print()
+    print("Janis Joplin")
+    print()
+
+elif feeling == "b":
+    print()
+
